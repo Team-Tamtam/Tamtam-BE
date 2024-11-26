8fd8be351c4e0cabeae7b76cc85ef794acf6fb59
seohyun-lee <applemint2003@gmail.com>
2024-11-27 01:28:01 +0900

    fix: CategoryName 분류 수정

--- mooney/src/main/java/tamtam/mooney/entity/CategoryName.java
+++ mooney/src/main/java/tamtam/mooney/entity/CategoryName.java
@@ -6,15 +6,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CategoryName {
-    FOOD("식비", "🍚"),
-    TRANSPORT("교통", "🚗"),
+    EVENTS_AND_GIFTS("경조/선물", "🎁"),
     EDUCATION("교육/학습", "📚"),
+    TRANSPORT("교통", "🚗"),
     FINANCE("금융", "💸"),
     ENTERTAINMENT("문화/여가", "🎭"),
     PET("반려동물", "🐶"),
     BEAUTY("뷰티/미용", "💅"),
     LIFESTYLE("생활", "🏠"),
     NIGHTLIFE("술/유흥", "🍻"),
+    FOOD("식비", "🍚"),
     TRAVEL("여행/숙박", "✈️"),
     ONLINE_SHOPPING("온라인 쇼핑", "🛒"),
     HEALTH("의료/건강", "💉"),
@@ -23,8 +24,7 @@ public enum CategoryName {
     HOUSING("주거/통신", "🏡"),
     CAFE("카페/간식", "☕"),
     FASHION("패션/쇼핑", "👗"),
-    EXTRA("기타", "📁")
-    ;
+    EXTRA("기타", "📁");
 
     private final String description;
     private final String icon;
